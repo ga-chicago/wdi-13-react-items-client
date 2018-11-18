7247aea024923b7054e6db23a772537d8e7686d5
Reuben Ayres <ra@sent.com>
2018-11-17 21:01:37 -0600

    you can add items :)

--- src/List.js
+++ src/List.js
@@ -4,7 +4,8 @@ class List extends Component {
   constructor() {
     super()
     this.state = {
-      items: []
+      items: [],
+      contentToAdd: ''
     }
   }
   async getItems() {
@@ -18,7 +19,40 @@ class List extends Component {
     })
   }
   componentDidMount() { 
+    console.log("componentDidMount List");
     this.getItems();
+  }
+  handleChange = (e) => {
+    this.setState({
+      contentToAdd: e.currentTarget.value
+    })
+  }
+  handleSubmit = (e) => {
+    e.preventDefault();
+    this.addItem();
+  }
+  async addItem() {
+    // take what's in state
+    // add it with fetch
+    const response = await fetch('http://localhost:9292/api/items', {
+      method: 'POST',
+      credentials: 'include',
+      body: JSON.stringify({
+        content: this.state.contentToAdd
+      }),
+      headers: { // this is how you set HTTP headers in fetch
+        'Content-Type': 'application/json'
+      }
+    })
+    const parsed = await response.json();
+    // if it works
+    if(parsed.status === 200) {
+      // clear out contentToAdd in state
+      this.setState({
+        contentToAdd: ''
+      })
+      this.getItems();
+    }
   }
   render(){
     const items = this.state.items.map((item, i) => {
@@ -26,6 +60,16 @@ class List extends Component {
     })
     return (
       <div>
+        <form onSubmit={this.handleSubmit}>
+          <input 
+            type="text" 
+            name="content" 
+            value={this.state.contentToAdd} 
+            placeholder="enter yr item ASAP"
+            onChange={this.handleChange}
+          />
+          <button>Add it!</button>
+        </form>
         <h2>Item List</h2>
         <ul>{items}</ul>
       </div>
